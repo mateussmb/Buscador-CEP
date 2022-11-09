5ada9f877570a6ade4ded905b522afec7cd5673e
mateussmb <mt.moreirabarros@gmail.com>
2022-11-09 14:17:08 -0300

    “Buscador”

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import  { FiSearch} from 'react-icons/fi';
+import './styles.css';
+import api from './services/api';
+import {MdCopyright} from 'react-icons/md';
+
+
+
 
 function App() {
+
+    const [input, setInput] = useState("")
+    const [cep, setCep] = useState({});
+
+    async function handleSearch() {
+        if (input ==='') {
+          alert('Preencha o CEP!!')
+          return;
+        } 
+
+        try {
+          const response = await api.get(`${input}/json`);
+          setCep(response.data)
+          
+          
+          setInput('')
+
+        }catch{
+          alert('CEP INVÁLIDO!!')
+          setInput('')
+        }
+      
+
+    }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscar CEP</h1>
+      <div className="containerInput">
+        <input type='cep' maxLength={8} placeholder="Digite um cep..."
+        value={input} onChange={(e) => setInput(e.target.value)}></input>
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color='#000'></FiSearch>
+        </button>
+
+      </div>
+
+      <main className='main'>
+
+      <h2>CEP: {cep.cep}</h2>
+      <span >{cep.logradouro}</span>
+      <span> {cep.localidade}</span>
+      <span>{cep.bairro}  </span>
+      <span></span>
+
+
+      </main>
+
+      <folder className='rodape'>
+        <MdCopyright  size={25}></MdCopyright>
+        <h2>mt.moreirabarros@gmail.com</h2>
+
+      </folder>
     </div>
+
+
   );
 }
 
-export default App;
+export default App; 
---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+// https://viacep.com.br/ws
+const api = axios.create({
+    baseURL: "https://viacep.com.br/ws/"
+})
+
+export default api
